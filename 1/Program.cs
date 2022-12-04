ad58a52a782608ec5aebc71f48063f89dfa83fea
Tatyana <tatyana.volkova1984@mail.ru>
2022-12-04 20:49:36 +0500

    перевод из строки в массив несколько значений

---
+++ 1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Linq;
+
+string text = "(1,2) (2,3) (4,5) (6,7)"
+            .Replace("(", "")
+            .Replace(")", "")
+            ; 
+Console.WriteLine(text);
+var data = text.Split(" ")
+            .Select(item => item.Split(','))
+            .Select(e => (int.Parse(e[0]), int.Parse(e[1])))
+            .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+     Console.WriteLine(data[i]);
+     Console.WriteLine();
+}
